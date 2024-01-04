d0f8cdd4fb27ede2fa0e07332215656b025ad076
shivaADSULE <shadowshivaadsule@gmail.com>
2024-01-04 22:16:25 +0530

    Implemented UI and basic controls.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,24 +1,55 @@
-﻿using System.Text;
+﻿using System.Windows.Controls;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Hc = HandyControl.Controls;
 
-namespace Snap_Screen
+
+namespace Snap_Screen;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Hc.Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    private Point startPoint;
+    private bool resizing = false;
+
+    public MainWindow()
     {
-        public MainWindow()
+        InitializeComponent();
+    }
+
+    private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+        startPoint = e.GetPosition(this);
+        resizing = true;
+        Mouse.Capture((UIElement)sender);
+    }
+
+    private void Border_MouseMove(object sender, MouseEventArgs e)
+    {
+        if (resizing)
         {
-            InitializeComponent();
+            Point endPoint = e.GetPosition(this);
+            double deltaX = endPoint.X - startPoint.X;
+            double deltaY = endPoint.Y - startPoint.Y;
+
+            Border border = (Border)sender;
+            border.Width += deltaX;
+            border.Height += deltaY;
+
+            startPoint = endPoint;
         }
+    }
+
+    private void Border_MouseUp(object sender, MouseButtonEventArgs e)
+    {
+        resizing = false;
+        Mouse.Capture(null);
+    }
+
+    private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        this.DragMove();
     }
 }
